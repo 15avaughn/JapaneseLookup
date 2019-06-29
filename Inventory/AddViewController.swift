e7b493c306a06ea874a695fc0ae103310c0f607c
Austin Vaughn <15avaughn@gmail.com>
2019-06-29 16:44:03 -0600

    Started

---
+++ Inventory/AddViewController.swift
@@ -0,0 +1,34 @@
+//
+//  AddViewController.swift
+//  Inventory
+//
+//  Created by Austin Vaughn on 6/29/19.
+//  Copyright © 2019 Austin Vaughn. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        let save = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(saveItem))
+        self.navigationItem.rightBarButtonItem = save
+    }
+    
+    @objc func saveItem() {
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
