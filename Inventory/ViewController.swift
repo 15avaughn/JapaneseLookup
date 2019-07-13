f6ba23fb5f1c5746e95ef6d1850d5fa844e63eb6
Austin Vaughn <15avaughn@gmail.com>
2019-07-13 14:07:42 -0600

    Done.

--- Inventory/ViewController.swift
+++ Inventory/ViewController.swift
@@ -7,8 +7,16 @@
 //
 
 import UIKit
+import SQLite3
 
 var itemBeingEdited: Int!
+var db: OpaquePointer?
+let queryString = "SELECT * FROM Items"
+let insertString = "INSERT INTO Items (shortDescription, longDescription) VALUES (?,?)"
+var stmt: OpaquePointer?
+
+let fileURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+    .appendingPathComponent("Inventory.sqlite")
 
 struct Item {
     var shortDesc : String
@@ -62,6 +70,33 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.title = "Inventory"
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self,
+                                       selector: #selector(saveToDatabase(_:)),
+                                       name: UIApplication.willResignActiveNotification,
+                                       object: nil)
+        
+        if sqlite3_open(fileURL.path, &db) != SQLITE_OK {
+            print("Error opening database.")
+        }
+        
+        if sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS Items (id INTEGER PRIMARY KEY AUTOINCREMENT, shortDescription VARCHAR, longDescription VARCHAR)", nil, nil, nil) != SQLITE_OK{
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("Error creating table: \(errmsg)")
+        }
+        
+        if sqlite3_prepare(db, queryString, -1, &stmt, nil) != SQLITE_OK{
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("Error preparing insert: \(errmsg)")
+            return
+        }
+        
+        while(sqlite3_step(stmt) == SQLITE_ROW){
+            let shortDescription = String(cString: sqlite3_column_text(stmt, 1))
+            let longDescription = String(cString: sqlite3_column_text(stmt, 2))
+            
+            items.append(Item(shortDesc: shortDescription, longDesc: longDescription))
+        }
     }
     
      // MARK: - Navigation
@@ -83,6 +118,45 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         }
      }
  
+    @objc func saveToDatabase(_ notification:Notification){
+        if sqlite3_open(fileURL.path, &db) != SQLITE_OK {
+            print("Error opening database.")
+        }
+        
+        if sqlite3_exec(db, "DELETE FROM Items", nil, nil, nil) != SQLITE_OK{
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("Error deleting table: \(errmsg)")
+        }
+        
+        for item in items{
+            if sqlite3_prepare(db, insertString, -1, &stmt, nil) != SQLITE_OK{
+                let errmsg = String(cString: sqlite3_errmsg(db)!)
+                print("Error preparing insert: \(errmsg)")
+                return
+            }
+            
+            //binding the parameters
+            if sqlite3_bind_text(stmt, 1, (item.shortDesc as NSString).utf8String, -1, nil) != SQLITE_OK{
+                let errmsg = String(cString: sqlite3_errmsg(db)!)
+                print("failure binding shortDesc: \(errmsg)")
+                return
+            }
+            
+            if sqlite3_bind_text(stmt, 2, (item.longDesc as NSString).utf8String, -1, nil) != SQLITE_OK{
+                let errmsg = String(cString: sqlite3_errmsg(db)!)
+                print("failure binding longDesc: \(errmsg)")
+                return
+            }
+            
+            //executing the query to insert values
+            if sqlite3_step(stmt) != SQLITE_DONE {
+                let errmsg = String(cString: sqlite3_errmsg(db)!)
+                print("Failure inserting item: \(errmsg)")
+                return
+            }
+        }
+        sqlite3_close(db)
+    }
     
 }
 
